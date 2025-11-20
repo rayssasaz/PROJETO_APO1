ba7b04bf4988b50492d073a56306287c1fd03a83
rayssasaz <rayssaazevedo31@gmail.com>
2025-11-20 19:02:04 -0300

    alterada a classe Usuario

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -1,5 +1,53 @@
 package model;
 
 public class Usuario {
+	private String nome;
+	private String telefone;
+	private String email;
+	private String cpf;
+	private String senha;
+	private Integer codUsuario;
+	
+	// getters e setters
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public Integer getCodUsuario() {
+		return codUsuario;
+	}
+	public void setCodUsuario(Integer codUsuario) {
+		this.codUsuario = codUsuario;
+	}
+	
+	
+	
 
 }
